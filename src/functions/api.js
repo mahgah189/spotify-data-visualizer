72229a3b85726dee910d95a3eacf22ea07b81056
TeamRocketStudify <mkbk189@gmail.com>
2024-05-28 19:57:21 -0700

    figured out how to retrieve song characteristics + audio analysis

--- src/functions/api.js
+++ src/functions/api.js
@@ -1,25 +1,50 @@
 const clientId = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+const redirectUrl = "http://localhost:5173";
+
+const authorizationEndpoint = "https://accounts.spotify.com/authorize";
+const tokenEndpoint = "https://accounts.spotify.com/api/token";
+const scope = 'user-read-private user-read-email';
+
+// Data structure that manages the current active token, caching it in localStorage
+// const currentToken = {
+//   get access_token() { return localStorage.getItem('access_token') || null; },
+//   get refresh_token() { return localStorage.getItem('refresh_token') || null; },
+//   get expires_in() { return localStorage.getItem('refresh_in') || null },
+//   get expires() { return localStorage.getItem('expires') || null },
+
+//   save: function (response) {
+//     const { access_token, refresh_token, expires_in } = response;
+//     localStorage.setItem('access_token', access_token);
+//     localStorage.setItem('refresh_token', refresh_token);
+//     localStorage.setItem('expires_in', expires_in);
+
+//     const now = new Date();
+//     const expiry = new Date(now.getTime() + (expires_in * 1000));
+//     localStorage.setItem('expires', expiry);
+//   }
+// };
 
 async function getSpotifyAccessToken() {
-  const url = "https://accounts.spotify.com/api/token";
   const headers = {
     "Content-Type": "application/x-www-form-urlencoded"
   };
   const body = `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`;
 
   try { 
-    const response = await fetch(url, {
+    const response = await fetch(tokenEndpoint, {
       method: "POST",
       headers: headers,
       body: body,
       redirect: 'follow'
     });
-    const data = await response.json();
+    return await response.json();
   } catch(error) {
     console.log(error)
   };
 
 };
 
-export default getSpotifyAccessToken;
+const data = await getSpotifyAccessToken();
+
+export default data;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-import getSpotifyAccessToken from "/src/functions/api.js";
+import token from "/src/functions/api.js";
 
 function Home() {
-  async function getAccessToken() {
-    const token = getSpotifyAccessToken();
-    console.log(token);
-    return token;
-  }
 
-  getAccessToken();
-
+  fetch("https://api.spotify.com/v1/audio-features/7aGcgv4G1upG0XK6nHC5rF", {
+    headers: {
+      "Authorization": `Bearer ${token.access_token}`
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      fetch("https://api.spotify.com/v1/audio-analysis/7aGcgv4G1upG0XK6nHC5rF", {
+        headers: {
+          "Authorization": `Bearer ${token.access_token}`
+        }
+      })
+        .then(response => response.json())
+        .then(data => console.log(data))
+    })
+  
   return (
     <></>
   )
