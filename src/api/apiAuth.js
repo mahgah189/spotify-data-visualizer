449768f5246c7dcfec3706df1caa83a6ae473898
TeamRocketStudify <mkbk189@gmail.com>
2024-07-06 15:55:37 -0400

    set up cloud functions successfully

--- src/api/apiAuth.js
+++ src/api/apiAuth.js
@@ -1,30 +1,44 @@
-const clientId = import.meta.env.VITE_CLIENT_ID;
-const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
-const redirectUrl = "http://localhost:5173";
+// const clientId = import.meta.env.VITE_CLIENT_ID;
+// const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+import { initializeApp } from 'firebase/app';
+import { getFunctions, httpsCallable } from 'firebase/functions';
 
-const authorizationEndpoint = "https://accounts.spotify.com/authorize";
-const tokenEndpoint = "https://accounts.spotify.com/api/token";
-const scope = 'user-read-private user-read-email';
+// const authorizationEndpoint = "https://accounts.spotify.com/authorize";
+// // const tokenEndpoint = "https://accounts.spotify.com/api/token";
+// const scope = 'user-read-private user-read-email';
 
-async function getSpotifyAccessToken() {
-  const headers = {
-    "Content-Type": "application/x-www-form-urlencoded"
-  };
-  const body = `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`;
+// async function getSpotifyAccessToken() {
+//   const headers = {
+//     "Content-Type": "application/x-www-form-urlencoded"
+//   };
+//   const body = `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`;
 
-  try { 
-    const response = await fetch(tokenEndpoint, {
-      method: "POST",
-      headers: headers,
-      body: body,
-      redirect: 'follow'
-    });
-    return await response.json();
-  } catch(error) {
-    console.log(error)
-  };
+//   try { 
+//     const response = await fetch(tokenEndpoint, {
+//       method: "POST",
+//       headers: headers,
+//       body: body,
+//       redirect: 'follow'
+//     });
+//     return await response.json();
+//   } catch(error) {
+//     console.log(error)
+//   };
+// };
 
-};
+let token;
+
+const app = initializeApp({
+  projectId: "statify-3b944",
+  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
+  authDomain: "statify-3b944.firebaseapp.com"
+});
+const functions = getFunctions(app);
+const getSpotifyToken = httpsCallable(functions, "getSpotifyToken");
+getSpotifyToken()
+  .then((result) => {
+    token = result.data;
+  });
 
 async function getCanvasAccessToken() {
   try {
@@ -35,7 +49,6 @@ async function getCanvasAccessToken() {
   }
 }
 
-const token = await getSpotifyAccessToken();
 const canvasToken = await getCanvasAccessToken();
 
 export { token, canvasToken };
